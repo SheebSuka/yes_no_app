6c17b5bcfb7845f30559bc7c13affae811f97e04
Sheeb <thingisss89@gmail.com>
2025-01-01 22:38:14 -0700

    Se creo el acceso a los mensajes en el chat.

--- lib/presentation/widgets/chat/her_message_bubble.dart
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -8,20 +8,22 @@ class HerMessageBubble extends StatelessWidget {
     final colors = Theme.of(context).colorScheme;
 
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Container(
           decoration: BoxDecoration(
-              color: colors.primary, borderRadius: BorderRadius.circular(20)),
+              color: colors.secondary, borderRadius: BorderRadius.circular(20)),
           child: const Padding(
             padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
             child: Text(
-              'Minim mollit ',
+              'Hello there Little People',
               style: TextStyle(color: Colors.white),
             ),
           ),
         ),
-        const SizedBox(height: 10)
+        const SizedBox(height: 5)
+
+        //todo: Image
       ],
     );
   }
